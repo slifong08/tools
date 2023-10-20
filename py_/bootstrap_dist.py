c363c7544b13c1dce9e4320df4407fa5860fe29d
slifong08 <slfong08@gmail.com>
2023-10-20 16:15:53 -0700

    quantile-compatible bootstrap script

--- py_/bootstrap_dist.py
+++ py_/bootstrap_dist.py
@@ -2,14 +2,15 @@ import os, sys
 import numpy as np
 import pandas as pd
 
-def bootstrap(data_list, size):  
+def bootstrap(data_list, size, stat):  
     
     """
-    return the discrete and relative 95% confidence intervals of a data_list 
+    return the discrete and 95% confidence intervals for a stat from a data_list 
     
     input
         list of data (list, continuous values) - any list of data values, must be continuous values. 
         size (int) - size of dataset to bootstrap from list. If None, make bootstrapped distribution from entire list
+        stat (float | str) - quantile to bootstrap (float, 0-1) | mean (str)
         
     method 
         1. If size is None, get the length of the list 
@@ -29,24 +30,36 @@ def bootstrap(data_list, size):
         size = len(data_list) # size of distribution to bootstrap
 
     #2
-    obs_stat = np.median(data_list) # get observed stat
+    if type(stat) is float:
+        obs_stat = np.quantile(data_list, stat) # get observed stat
+        
+    elif stat=="mean":
+        obs_stat = np.mean(data_list) # get observed stat
     
     #3
     nboot = 10000 # resample 10000 times
-    val = 0
+    val = 0  # resample count
     bs_stats = []
     
     #4
     while val < nboot:
 
-        bs_dist = np.random.choice(data_list, replace = True, size = size)
+        bs_dist = np.random.choice(data_list, replace=True, size=size)
         
         #5
-        bs_stat = np.median(bs_dist)
-        bs_stats.append(bs_stat)
-        val +=1
+        if type(stat) is float:
+            bs_stat = np.quantile(bs_dist, stat)
+            
+        elif stat=="mean":
+            bs_stat = np.mean(bs_dist)
+            
+        bs_stats.append(bs_stat)  # add stat to list
+        val +=1  # count resample
+        
     #6
-    bs = pd.DataFrame(data = bs_stats, index = np.arange(nboot), columns = ["bs_stat"]) # make dataframe of bootstraps
+    bs = pd.DataFrame(data = bs_stats, 
+                      index = np.arange(nboot), 
+                      columns = ["bs_stat"]) # make dataframe of bootstraps
 
     #7 center the stat distribution
     bs["deltas"] = bs["bs_stat"] - obs_stat
@@ -57,10 +70,15 @@ def bootstrap(data_list, size):
     #9  get discrete 95th CI
     low = bs.deltas.quantile(0.025) 
     high = bs.deltas.quantile(0.975)
-    ci_discrete = [high, low]
+    ci_relative = [high, low]  # assume obs value is centered at zero
 
     #10  return ci relative to observed stat 
-    ci_relative = obs_stat - [high, low]
+    ci_discrete = obs_stat - [high, low]  # assume obs value is center
    
-    print("CI", ci_discrete, ci_relative, obs_stat)
+    print(f"measure bootstrap CI of {stat} \
+          quantile| mean estimate\n observed {stat} value:", 
+          obs_stat,  
+          "\ndiscrete diff from observed:", ci_discrete, 
+          "\nrelative diff from observed:", ci_relative)
+    
     return ci_discrete, ci_relative
